857f7141664a396bfc3ef43115f91e5565b0002c
Shafayet <shafayetshafee@gmail.com>
2022-05-19 21:31:41 +0600

    adding tests to the package

--- R/compute_corr.R
+++ R/compute_corr.R
@@ -12,13 +12,29 @@
 #'
 #' @importFrom rlang .data
 compute_corr <- function(data, var1, var2) {
-  stats::cor.test(
-    x = data %>% dplyr::pull({{var1}}),
-    y = data %>% dplyr::pull({{var2}})
+  var1_chr <- rlang::as_label(rlang::ensym(var1))
+  var2_chr <- rlang::as_label(rlang::ensym(var2))
+
+  if (!(var1_chr %in% names(data))) {
+    # stop(glue::glue("{var1_chr} is not in the data-set."))
+    usethis::ui_stop("{var1_chr} is not in the data-set.")
+  }
+
+  if (!(var2_chr %in% names(data))) {
+    # stop(glue::glue("{var2_chr} is not in the data-set."))
+    usethis::ui_stop("{var2_chr} is not in the data-set.")
+  }
+
+  result <- stats::cor.test(
+    x = data %>% dplyr::pull({{ var1 }}),
+    y = data %>% dplyr::pull({{ var2 }})
   ) %>%
     broom::tidy() %>%
     dplyr::select(
       correlation = .data$estimate,
       pval = .data$p.value
     )
+
+  attr(result$correlation, "names") <- NULL
+  return(result)
 }
